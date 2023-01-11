24a62056350f38535b2940fdbc449ab0f669d8e7
Bathulah <bathulahmahir@gmail.com>
2023-01-11 14:03:55 +0800

    add load task functionality

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,45 @@
 import { v4 as uuidV4 } from "uuid"
 
 
-type Task =  {
+type Task = {
   id: string
   title: string
-  completed: boolean 
+  completed: boolean
   createdAt: Date
 };
 
 const list = document.querySelector<HTMLUListElement>("#list");
 const form = document.getElementById("new-task-form") as HTMLFormElement | null;
 const input = document.querySelector<HTMLInputElement>("#new-task-title");
+
 const tasks: Task[] = loadTasks()
+
 tasks.forEach(addListItem)
 
 // ? is called optional chaining
 
 form?.addEventListener("submit", e => {
-  
+
   e.preventDefault();
 
-  if (input?.value == "" || input?.value == null) return 
+  if (input?.value == "" || input?.value == null) return
 
-   const newTask = {
+  const newTask = {
     id: uuidV4(),
     title: input.value,
     completed: false,
     createdAt: new Date()
-   }
-   
-   tasks.push(newTask)
-   saveTasks()
+  }
 
-   addListItem(newTask)
-   input.value = "";
-   
+  tasks.push(newTask)
+  saveTasks()
+
+  addListItem(newTask)
+  input.value = "";
+
 })
 
-function addListItem(task : Task ) {
+function addListItem(task: Task) {
 
   const item = document.createElement("li")
   const label = document.createElement("label")
@@ -45,19 +47,25 @@ function addListItem(task : Task ) {
   checkbox.addEventListener("change", () => {
     task.completed = checkbox.checked;
     console.log(tasks)
-    
+
     saveTasks()
   })
   checkbox.type = "checkbox"
-  checkbox.checked = task.completed 
-  label.append(checkbox,task.title)
+  checkbox.checked = task.completed
+  label.append(checkbox, task.title)
   item.append(label)
   list?.append(item)
-  
+
 }
 
 function saveTasks() {
-  localStorage.setItem("TASKS",JSON.stringify(tasks))
+  localStorage.setItem("TASKS", JSON.stringify(tasks))
 }
 
+function loadTasks(): Task[] {
 
+  console.log("loaded ke dok")
+  const taskJson = localStorage.getItem("TASKS")
+  if (taskJson == null) return []
+  return JSON.parse(taskJson)
+}
